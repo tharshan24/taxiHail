c27ec8c321295f096e9b78970f4ff3b2d84b297e
Priyatharshan <tharshan24@gmail.com>
2023-06-08 11:27:40 +0530

    enable transaction management globally

--- TaxiHailCore/src/main/java/com/taxi/taxihailcore/TaxiHailCoreApplication.java
+++ TaxiHailCore/src/main/java/com/taxi/taxihailcore/TaxiHailCoreApplication.java
@@ -1,13 +1,24 @@
 package com.taxi.taxihailcore;
 
+import jakarta.persistence.EntityManagerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class TaxiHailCoreApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TaxiHailCoreApplication.class, args);
+	}
+
+	@Bean
+	public PlatformTransactionManager transactionManager(EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
 	}
 
 }
